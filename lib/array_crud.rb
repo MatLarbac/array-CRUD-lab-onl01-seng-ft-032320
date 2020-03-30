b326679953316b4649ea6168de28e177382c6100
Mat Cabral <mathewcabral@gmail.com>
2020-03-30 19:38:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  create_an_empty_array = []
+   []
 end
 
 def create_an_array
-  create_an_array = ["Binh", "Tim", "Chris", "Mat"]
+  [2, 4, 6, 8]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -24,13 +24,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index(element)
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array.first
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array.last
+  array[-1]
 end
